75934bf8db6e6af14748ea839d2bfce0e89992ce
jonny1811 <jonnyda1811@gmail.com>
2022-03-08 23:11:27 -0300

    ensure LocalLoadPurchases returns emptu list on load failed

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -42,6 +42,13 @@ export class CacheStoreSpy implements CacheStore {
             throw new Error()
         })
     }
+
+    simulatefetchError (): void {
+        jest.spyOn(CacheStoreSpy.prototype, 'fetch').mockImplementationOnce(() => {
+            this.actions.push(CacheStoreSpy.Action.fetch)
+            throw new Error()
+        })
+    }
 }
 
 export namespace CacheStoreSpy {
